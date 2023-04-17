5df08bd20ea03cf2b0236938a4a005fa22ab307d
Rajat Sharma <35336213+rotroapp@users.noreply.github.com>
2023-04-17 21:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
-    //Write your code here
-  
+    
+    let rad = document.querySelector('#radius').value;
+	let vol = document.querySelector('#volume');
+     const cal = rad*rad*rad;
+	 vol.value = cal;
+	event.preventDefault();
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
